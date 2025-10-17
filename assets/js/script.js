d42dc9eed8ee32dee08b36338034b38a6f9a7b59
Yannick-lep <lepinayyannick7@gmail.com>
2025-10-17 16:57:38 +0200

    re

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,13 @@ fetch("./assets/js/data.json")
     data.results.forEach(
       user => {
         outputHtml += `
+        <div class="diapo">
         <figure class="user">
           <h4>${user.name.first} ${user.name.last}</h4>
           <img src="${user.picture.thumbnail}" alt="${user.name.first} ${user.name.last}">
           <figcaption>${user.email}</figacaption>
         </figure>
+        </div>
         `;
       }
     );
