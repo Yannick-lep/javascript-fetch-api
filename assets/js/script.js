4acb9f2b45f98e4b1c2b2cc419ea20acbcdaafa2
Yannick-lep <lepinayyannick7@gmail.com>
2025-10-17 16:44:06 +0200

    création data.json affichage nom prenom photo email + style css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,19 @@ fetch("./assets/js/data.json")
   .then((reponse) => reponse.json())
   .then((data) => {
     console.log(data);
+    let outputHtml = '';
+    data.results.forEach(
+      user => {
+        outputHtml += `
+        <figure class="user">
+          <h4>${user.name.first} ${user.name.last}</h4>
+          <img src="${user.picture.thumbnail}" alt="${user.name.first} ${user.name.last}">
+          <figcaption>${user.email}</figacaption>
+        </figure>
+        `;
+      }
+    );
+    document.getElementById("users").innerHTML = outputHtml
   })
   .catch((error) => {
     console.log(error);
